c43bdd1cd9e2b5d9ab6d961d265fb069272f97a8
Chris <mortredbarylen@gmail.com>
2013-04-04 19:11:41 -0400

    Fixed deployment for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var io = require('socket.io'),
   connect = require('connect'),
   chatter = require('chatter');
 
-var app = connect().use(connect.static('public')).listen(8000);
+var app = connect().use(connect.static('public')).listen(process.env.PORT || 3000);
 var chat_room = io.listen(app);
-
+chat_room.configure(function() {
+	chat_room.set("transports", ["xhr-polling"]);
+	chat_room.set("polling duration", 10);
+});
 chatter.set_sockets(chat_room.sockets);
 
 chat_room.sockets.on('connection', function (socket) {
